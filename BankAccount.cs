47d4f8a7af0c7063c85fa3c12f9b84722706d84a
Santosh Marigowda <santosharakere@gmail.com>
2020-03-17 11:32:06 +0000

    compute balance using all transactions

--- BankAccount.cs
+++ BankAccount.cs
@@ -1,13 +1,28 @@
 ﻿using System;
+using System.Collections.Generic;
+
 namespace learn_dotnet_classes_objects
 {
     public class BankAccount
     {
+        private List<Transaction> allTransactions = new List<Transaction>();
         public string Number { get; }
         public string Owner { get; set; }
-        public decimal Balance { get; }
+
+        public decimal Balance
+        {
+            get
+            {
+                decimal balance = 0;
+                foreach(var item in allTransactions) {
+                    balance += item.Amount;
+                }
+                return balance;
+            }
+        }
+
         private static int accountNumberSeed = 1234567890;
-        private List<Transaction> allTransactions = new List<Transaction>();
+
 
         public void MakeDeposit(decimal amount, DateTime date, string note)
         {
